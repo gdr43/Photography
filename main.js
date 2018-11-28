5b924ff1247817a21930a929161c5187cd4cb826
gdr43 <dilip.gujju@gmail.com>
2018-11-28 19:22:16 +0530

    Modified html and js

---
+++ main.js
@@ -0,0 +1,28 @@
+<script>
+
+            
+            
+            
+            var actors = 'pavankalyan';
+            switch(actors)
+                {
+                    case "prabas":
+                        alert('Rebel star');
+                        break;
+                    case "allu arjun":
+                        alert('stylish star');
+                        break;
+                    case "pavankalyan":
+                        alert('powerstar');
+                        break;
+                    default:
+                        alert('star');
+                        break;
+                        
+                        
+                }
+            
+    
+    
+    
+</script>
